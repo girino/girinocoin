e58905eea80ce05e0415a30bf1d739156a62a1cd
Girino Vey <girino@Girinos-Mac-mini.local>
2019-01-28 21:19:35 -0200

    corrected pubkeyscript base58 prefix to lowercase g

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,7 +132,7 @@ public:
         //vSeeds.emplace_back("dnsseed.girinocoinpool.org");
         //vSeeds.emplace_back("dnsseed.koin-project.com");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,39);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,97);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
